9a77c9dc57bd9f3bb513e7c6c7ec84ea839c0804
sergeySchmidt <draggsbonys@gmail.com>
2022-06-24 19:05:04 +0300

    24.06.2022 19:00

--- UNIT 13. Associative array (object)/sctipt.js
+++ UNIT 13. Associative array (object)/sctipt.js
@@ -132,3 +132,64 @@ function f10 (arr, value) {
   }
 }
 console.log(f10(a10, 2)); //true
+//11 При нажатии b-11 выполняете функцию f11. Функция должна получить ключ из i-11 и удалить запись из массива a11 с таким ключем. После этого вывести массив в out-11. Для вывода используйте функцию f5.
+let a11 = {'one': 1, 'two': 2, 'three': 3};
+function f11 () {
+  let inputKey = document.querySelector('.i-11').value;
+  delete a11[inputKey];  
+  f5(a11, '.out-11');
+} 
+function f5 (arr, block) {
+  let out = '';
+  for (key in arr) {
+    out += `${key} ${arr[key]} <br>`
+  }
+  document.querySelector(block).innerHTML = out;
+}
+document.querySelector('.b-11').onclick = f11; 
+//12 При нажатии b-12 выполняете функцию f12. Функция должна получить значение из i-12 и удалить запись из массива a12 с таким значением. После этого вывести массив в out-12. Для вывода используйте функцию f5.
+let a11 = {'one': 1, 'two': 2, 'three': 3};
+function f11 () {
+  let inputValue = document.querySelector('.i-11').value;
+  for (let key in a11) {
+    if (inputValue == a11[key]){
+      delete a11[key];
+    }
+  }
+  f5(a11, '.out-11');
+} 
+function f5 (arr, block) {
+  let out = '';
+  for (key in arr) {
+    out += `${key} ${arr[key]} <br>`
+  }
+  document.querySelector(block).innerHTML = out;
+}
+document.querySelector('.b-11').onclick = f11; 
+//13 При нажатии b-13 выполняете функцию f13. Функция должна в out-13 выводить сумму значений массива a13 (только числа).
+let a13 = {'John': 100,'Ash': 'none','Mike': 150,'Piter': 'none'};
+let sum = 0;
+function f13 () {
+  for (let key in a13) {
+    if (typeof a13[key] == 'number') {
+      sum += a13[key];
+    }
+  }
+  document.querySelector('.out-13').innerHTML = sum;
+}
+document.querySelector('.b-13').onclick = f13;
+//14 При нажатии b-14 выполняете функцию f14. Функция должна в out-14 выводить нулевые (по индексу) элементы вложенных массивов в a14. Вывод через пробел.
+let a14 = {
+  'one': {'one': 1, 'two': 2, 'three': 3},
+  'two': {'four': 4, 'five': 5, 'six': 6},
+  'three': {'seven': 7, 'eigth': 8, 'nine': 9},  
+}
+function f14 () {
+  let out = '';
+  for (let key in a14) {
+    let subObj = a14[key];
+    out += `${Object.keys(subObj)[0]} ${Object.values(subObj)[0]}<br>`; 
+  }
+  document.querySelector('.out-14').innerHTML = out;
+}
+document.querySelector('.b-14').onclick = f14;
